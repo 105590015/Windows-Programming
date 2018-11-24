58943687cd53a004a3d6cc98a847f833fff7d75e
a105590015 <t105590015@ntut.org.tw>
2018-11-24 21:14:49 +0800

    hw1

---
+++ Homework1/POSCustomerSideForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Homework1
+{
+    public partial class POSCustomerSideForm : Form
+    {
+        private POSCustomerSideModel _model = new POSCustomerSideModel();
+        private List<Button> _buttonList = new List<Button>();
+        private int[] _buttonLocationX = new int[] { 6, 100, 193 };
+        private int[] _buttonLocationY = new int[] { 21, 120, 219 };
+        const String END = "\r\n";
+        const String UNIT = "元";
+        const int BUTTONS = 9;
+        const int ROW = 3;
+        const int COLUMN = 3;
+        public POSCustomerSideForm()
+        {
+
+            InitializeComponent();
+            _model.CreateInitialMeals();
+            _label1.Text = _model.GetPageInformation();
+            for (int i = 0; i < BUTTONS; i++)
+            {
+                Button button = new Button();
+                Meal meal = _model.GetMealList()[i];
+                button.Text = meal.GetName() + END + meal.GetPrice() + UNIT;
+                button.Font = new Font("Microsoft JhengHei", 9F);
+                button.Size = new Size(90, 95);
+                button.Location = new Point(_buttonLocationX[i % COLUMN], _buttonLocationY[i / ROW]);
+                button.TabIndex = i;
+                button.Click += ClickMenuButton;
+                _buttonList.Add(button);
+                _groupBox1.Controls.Add(button);
+            }
+        }
+
+        //餐點按鈕被觸發
+        private void ClickMenuButton(object sender, EventArgs e)
+        {
+            _model.SelectMeal(((Button)(sender)).TabIndex);
+            _button12.Enabled = true;
+        }
+
+        //加點按鈕被觸發
+        private void ClickAddButton(object sender, EventArgs e)
+        {
+            DataGridViewRowCollection rows = _dataGridView1.Rows;
+            Meal meal = _model.GetSelectedMeal();
+            String mealName;
+            int mealPrice;
+            mealName = meal.GetName();
+            mealPrice = meal.GetPrice();
+            rows.Add(new Object[] { mealName, mealPrice.ToString() + UNIT });
+            _label2.Text = _model.GetTotalPrice(mealPrice);
+        }
+
+        //換頁按鈕被觸發
+        private void ClickChangedPageButton(object sender, EventArgs e)
+        {
+            _model.ChangePage(((Button)(sender)).TabIndex);
+            ResetMealButton();
+            _button10.Enabled = _model.EnablePreviousButton();
+            _button11.Enabled = _model.EnableNextButton();
+            _label1.Text = _model.GetPageInformation();
+        }
+
+        //重設菜單按鈕資料
+        private void ResetMealButton()
+        {
+            for (int i = 0; i < BUTTONS; i++)
+            {
+                try
+                {
+                    Meal meal = _model.GetMeal(i);
+                    _buttonList[i].Text = meal.GetName() + END + meal.GetPrice() + UNIT;
+                    _buttonList[i].Visible = true;
+                }
+                catch
+                {
+                    _buttonList[i].Visible = false;
+                }
+            }
+        }
+    }
+}
---
+++ Homework1/POSCustomerSideModel.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework1
+{
+    class POSCustomerSideModel
+    {
+        private int _page = 0;
+        private Meal _selectedMeal = null;
+        private Order _order = new Order();
+        private List<Meal> _mealList = new List<Meal>();
+        const String PAGE = "Page：";
+        const String SLASH = " / ";
+        const String TOTAL = "Total：";
+        const String UNIT = "元";
+        const String MEAL1 = "大麥克";
+        const String MEAL2 = "牛肉吉士堡";
+        const String MEAL3 = "四盎司牛肉堡";
+        const String MEAL4 = "培根牛肉堡";
+        const String MEAL5 = "麥香魚";
+        const String MEAL6 = "麥香雞";
+        const String MEAL7 = "勁辣雞腿堡";
+        const String MEAL8 = "板烤雞腿堡";
+        const String MEAL9 = "起司豬排堡";
+        const String MEAL10 = "大大雞腿堡";
+        const String MEAL11 = "六塊麥克雞塊";
+        const String MEAL12 = "千島黃金蝦堡";
+        const String MEAL13 = "安格斯黑牛堡";
+        const String MEAL14 = "嫩煎雞腿堡";
+        const String MEAL15 = "辣脆雞腿堡";
+        const int BUTTONS = 9;
+        const int BASE = 10;
+        const int PRICE49 = 49;
+        const int PRICE53 = 53;
+        const int PRICE59 = 59;
+        const int PRICE69 = 69;
+        const int PRICE79 = 79;
+        const int PRICE99 = 99;
+        const int PRICE109 = 109;
+        // 建立基礎菜單
+        public void CreateInitialMeals()
+        {
+            String[] meals = new String[]
+            { MEAL1, MEAL2, MEAL3, MEAL4, MEAL5,
+                MEAL6, MEAL7, MEAL8, MEAL9, MEAL10,
+                MEAL11, MEAL12, MEAL13, MEAL14, MEAL15 };
+            int[] prices = new int[]
+            { PRICE69, PRICE59, PRICE79, PRICE99, PRICE49,
+                PRICE49, PRICE69, PRICE79, PRICE53, PRICE53,
+                PRICE59, PRICE69, PRICE109, PRICE109, PRICE109 };
+            for (int i = 0; i < prices.Length; i++)
+                _mealList.Add(new Meal(meals[i], prices[i]));
+        }
+
+        //取得菜單
+        public List<Meal> GetMealList()
+        {
+            return _mealList;
+        }
+
+        //取得第index項餐點資料
+        public Meal GetMeal(int index)
+        {
+            return _mealList[_page * BUTTONS + index];
+        }
+
+        //儲存被點擊的餐點
+        public void SelectMeal(int whichButton)
+        {
+            int whichMeal = _page * BUTTONS + whichButton;
+            _selectedMeal = _mealList[whichMeal];
+        }
+
+        //取得被點擊的餐點
+        public Meal GetSelectedMeal()
+        {
+            return _selectedMeal;
+        }
+
+        //取得總金額資訊
+        public String GetTotalPrice(int price)
+        {
+            int prices;
+            _order.SetPrice(price);
+            prices = _order.GetPrices();
+            return TOTAL + prices.ToString() + UNIT;
+        }
+
+        //換頁
+        public void ChangePage(int buttonIndex)
+        {
+            _page += buttonIndex - BASE;
+        }
+
+        //控制上一頁按鈕enable
+        public bool EnablePreviousButton()
+        {
+            if (_page > 0)
+                return true;
+            else
+                return false;
+        }
+
+        //控制下一頁按鈕enable
+        public bool EnableNextButton()
+        {
+            if (_mealList.Count - (_page + 1) * BUTTONS > 0)
+                return true;
+            else
+                return false;
+        }
+
+        //取得頁碼資訊
+        public String GetPageInformation()
+        {
+            int totalPage;
+            if (_mealList.Count % BUTTONS == 0)
+                totalPage = _mealList.Count / BUTTONS;
+            else
+                totalPage = _mealList.Count / BUTTONS + 1;
+            return PAGE + (_page + 1).ToString() + SLASH + totalPage.ToString();
+        }
+    }
+}
